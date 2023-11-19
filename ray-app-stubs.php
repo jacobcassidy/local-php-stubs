a4f1e68ec9f35b2453d3121f4a2ccf1822341c15
Jacob Cassidy <jacob@jacobcassidy.com>
2023-11-19 21:25:32 +0700

    Add `ray-app-stubs.php`

---
+++ ray-app-stubs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Self-created stub declarations for Ray app functions
+ */
+
+namespace {
+    /**
+     * @param mixed ...$args
+     *
+     * @return object
+     */
+    function ray()
+    {
+    }
+}
